6f8f798c9305231bce8f57c19c68d55ba7ec1a6f
Rochana99 <116473568+Rochana99@users.noreply.github.com>
2025-07-07 22:05:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "eloSELjT#zIFJh-1EthSY6VmcT7dU_uTVM409W3RaEqlWehDUKjo",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Rochana99/ROCHANA-MD/blob/main/images/20250706_212935.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Rochana99/ROCHANA-MD/refs/heads/main/images/20250706_212935.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ROCHANA-MD Is Alive Now😍*",
 BOT_OWNER: '94766443560',  // Replace with the owner's phone number
 
