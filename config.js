bb376bc9f1706730280a11b7f042bdc6df24ff1d
Rochana99 <116473568+Rochana99@users.noreply.github.com>
2025-07-11 13:25:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eloSELjT#zIFJh-1EthSY6VmcT7dU_uTVM409W3RaEqlWehDUKjo",
+SESSION_ID: process.env.SESSION_ID || "3cZiwJbT#5pWJUF2o-aqFy-nhgtNiOE12gIBpFPd32skGW8Dx1c0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Rochana99/ROCHANA-MD/refs/heads/main/images/20250706_212935.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ROCHANA-MD Is Alive Now😍*",
 BOT_OWNER: '94766443560',  // Replace with the owner's phone number
